b508a630db2881abdc33061947eefdce6137cedf
bigode_10 <pierre.delphim@escola.pr.gov.br>
2026-09-24 11:10:38 -0300

    Update ex01.kt

--- ex01.kt
+++ ex01.kt
@@ -1,1 +1,10 @@
+fun main() {
+    println("=====================================")
+    println("MEU PRIMEIRO PROGRAMA")
+    println("=====================================")
+    println("Nome: Aluno Exemplo")
+    println("Turma: 3DS")
+    println("Escola: Alberto Gomes Veiga")
+    println("Bem-vindo ao Kotlin!")
+}
 
